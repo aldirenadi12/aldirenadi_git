d376c62fc728dc0eed0a31e7c6c897c910bfc638
Brian Voong <bhlvoong@gmail.com>
2018-07-17 14:50:13 -0700

    Ep 4 New Messages + Groupie + Picasso

--- app/src/main/java/com/letsbuildthatapp/kotlinmessenger/registerlogin/LoginActivity.kt
+++ app/src/main/java/com/letsbuildthatapp/kotlinmessenger/registerlogin/LoginActivity.kt
@@ -1,10 +1,11 @@
-package com.letsbuildthatapp.kotlinmessenger
+package com.letsbuildthatapp.kotlinmessenger.registerlogin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.letsbuildthatapp.kotlinmessenger.R
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
